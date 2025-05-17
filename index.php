c88bd247d4fa985a10022625b384fb707ca03695
Gloria Enyi <nkemaconam@gmail.com>
2025-05-17 17:19:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,235 @@
+<?php require_once('header.php'); ?>
+
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row)
+{
+    $cta_title = $row['cta_title'];
+    $cta_content = $row['cta_content'];
+    $cta_read_more_text = $row['cta_read_more_text'];
+    $cta_read_more_url = $row['cta_read_more_url'];
+    $cta_photo = $row['cta_photo'];
+    $featured_product_title = $row['featured_product_title'];
+    $featured_product_subtitle = $row['featured_product_subtitle'];
+    $total_featured_product_home = $row['total_featured_product_home'];
+    $total_latest_product_home = $row['total_latest_product_home'];
+    $total_popular_product_home = $row['total_popular_product_home'];
+    $home_service_on_off = $row['home_service_on_off'];
+    $home_welcome_on_off = $row['home_welcome_on_off'];
+    $home_featured_product_on_off = $row['home_featured_product_on_off'];
+    $home_latest_product_on_off = $row['home_latest_product_on_off'];
+    $home_popular_product_on_off = $row['home_popular_product_on_off'];
+
+}
+
+
+?>
+
+<div id="bootstrap-touch-slider" class="carousel bs-slider fade control-round indicators-line" data-ride="carousel" data-pause="hover" data-interval="false" >
+    <ol class="carousel-indicators">
+        <?php
+        $i=0;
+        $statement = $pdo->prepare("SELECT * FROM tbl_slider");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {            
+            ?>
+            <li data-target="#bootstrap-touch-slider" data-slide-to="<?php echo $i; ?>" <?php if($i==0) {echo 'class="active"';} ?>></li>
+            <?php
+            $i++;
+        }
+        ?>
+    </ol>
+    <div class="carousel-inner" role="listbox" >
+
+        <?php
+        $i=0;
+        $statement = $pdo->prepare("SELECT * FROM tbl_slider");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {            
+            ?>
+            <div class="item <?php if($i==0) {echo 'active';} ?>" style="background-image:url(assets/uploads/<?php echo $row['photo']; ?>);">
+                <div class="bs-slider-overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="slide-text <?php if($row['position'] == 'Left') {echo 'slide_style_left';} elseif($row['position'] == 'Center') {echo 'slide_style_center';} elseif($row['position'] == 'Right') {echo 'slide_style_right';} ?>">
+                            <h1 data-animation="animated <?php if($row['position'] == 'Left') {echo 'zoomInLeft';} elseif($row['position'] == 'Center') {echo 'flipInX';} elseif($row['position'] == 'Right') {echo 'zoomInRight';} ?>"><?php echo $row['heading']; ?></h1>
+                                                   </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            $i++;
+        }
+        ?>
+    </div>
+    <a class="left carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="prev" style="color: gold;">
+        <span class="fa fa-angle-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="next" style="color: gold;">
+        <span class="fa fa-angle-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+
+</div>
+
+
+
+<?php if($home_featured_product_on_off == 1): ?>
+<div class="product pt_70 pb_70" style="background-color: black;color: gold;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="headline">
+                    <h2><?php echo $featured_product_title; ?></h2>
+                    <h3><?php echo $featured_product_subtitle; ?></h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="product-carousel">
+                    
+                    <?php
+                    $statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_is_featured=? AND p_is_active=? LIMIT ".$total_featured_product_home);
+                    $statement->execute(array(1,1));
+                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+                    foreach ($result as $row) {
+                        ?>
+                        <div class="item">
+                            <div class="thumb">
+                                <div class="photo" style="background-image:url(assets/uploads/<?php echo $row['p_featured_photo']; ?>); image-rendering: high-quality;"></div>
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="text">
+                                <h3><a href="product.php?id=<?php echo $row['p_id']; ?>"><?php echo $row['p_name']; ?></a></h3>
+                                <h4>
+                                    N<?php echo $row['p_current_price']; ?> 
+                                </h4>
+                                <?php if($row['p_qty'] == 0): ?>
+                                    <div class="out-of-stock">
+                                        <div class="inner">
+                                            Out Of Stock
+                                        </div>
+                                    </div>
+                                <?php else: ?>
+                                    <p><a href="product.php?id=<?php echo $row['p_id']; ?>"><i class="fa fa-eye"></i> View Details</a></p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+
+<?php if($home_latest_product_on_off == 1): ?>
+<div id="featured" class="product pt_70 pb_30" style="background-color: black;color: gold;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="product-carousel">
+
+                    <?php
+                    $statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_is_active=? ORDER BY p_id DESC LIMIT ".$total_latest_product_home);
+                    $statement->execute(array(1));
+                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+                    foreach ($result as $row) {
+                        ?>
+                        <div class="item">
+                            <div class="thumb">
+                                <div class="photo" style="background-image:url(assets/uploads/<?php echo $row['p_featured_photo']; ?>);"></div>
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="text">
+                                <h3><a href="product.php?id=<?php echo $row['p_id']; ?>"><?php echo $row['p_name']; ?></a></h3>
+                                <h4>
+                                    N<?php echo $row['p_current_price']; ?> 
+                                </h4>
+                                <?php if($row['p_qty'] == 0): ?>
+                                    <div class="out-of-stock">
+                                        <div class="inner">
+                                            Out Of Stock
+                                        </div>
+                                    </div>
+                                <?php else: ?>
+                                    <p><a href="product.php?id=<?php echo $row['p_id']; ?>"><i class="fa fa-eye"></i> View Details</a></p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+
+<?php if($home_popular_product_on_off == 1): ?>
+<div class="product pt_70 pb_70" style="background-color: black;color: gold;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="product-carousel">
+
+                    <?php
+                    $statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_is_active=? ORDER BY p_total_view DESC LIMIT ".$total_popular_product_home);
+                    $statement->execute(array(1));
+                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+                    foreach ($result as $row) {
+                        ?>
+                        <div class="item">
+                            <div class="thumb">
+                                <div class="photo" style="background-image:url(assets/uploads/<?php echo $row['p_featured_photo']; ?>);"></div>
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="text">
+                                <h3><a href="product.php?id=<?php echo $row['p_id']; ?>"><?php echo $row['p_name']; ?></a></h3>
+                                <h4>
+                                    N<?php echo $row['p_current_price']; ?> 
+                                </h4>
+                                <?php if($row['p_qty'] == 0): ?>
+                                    <div class="out-of-stock">
+                                        <div class="inner">
+                                            Out Of Stock
+                                        </div>
+                                    </div>
+                                <?php else: ?>
+                                    <p><a href="product.php?id=<?php echo $row['p_id']; ?>"><i class="fa fa-eye"></i> View Details</a></p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+
+
+
+<?php require_once('footer.php'); ?>
